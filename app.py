807d0a186cff743251113ae0c7d5f2913ded9800
subhamivan <ivan.sarangi@gmail.com>
2024-10-04 15:38:35 +0530

    added config

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
+from dotenv import load_dotenv
 
 from models import db
 from routes import api
+from config import Config
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
     app.debug = True
-    app.config["SECRET_KEY"] = "opensecret"
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notifications.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_RECORD_QUERIES"] = True
+    # Load the configuration
+    app.config.from_object(Config)
 
     db.init_app(app)
     app.register_blueprint(api, url_prefix="/api")
 
     DebugToolbarExtension(app)
-    app.config["DEBUG_TB_PROFILER_ENABLED"] = True
+
     with app.app_context():
         db.create_all()
 
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+    DEBUG_TB_PROFILER_ENABLED = True
